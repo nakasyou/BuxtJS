82e7e3b4025b2c76f24535ec47ca4bcd51ec1dfe
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-04 17:21:23 +0900

    Create jsx.ts

---
+++ mod/jsx.ts
@@ -0,0 +1,9 @@
+import {
+  jsx
+} from "../mods/jsx/mod.ts"
+
+export const Fragment = jsx.Fragment
+export const h = jsx.h
+export const renderToString = jsx.renderToString
+export type Component = jsx.Component
+export type Node = jsx.Node
