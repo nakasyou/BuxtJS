2d6af64a08842aac5f37f0999e979d66d4791327
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-04 17:29:15 +0900

    Update app.ts

--- mods/luxt/src/app.ts
+++ mods/luxt/src/app.ts
@@ -1,11 +1,28 @@
+import * as jsx from "../deps.ts"
+
+export interface Context {
+  
+}
+export type Router = (ctx: Context) => (
+  Response |
+  Node
+)
+
+export const defineRoute = (router) => router
+
 export interface AppInit {
   imports: Record<string, any>
 }
 export class App {
+  init: AppInit
   constructor (init: AppInit) {
-    
+    this.init = AppInit
   }
   fetch (request: Request): Response {
-    return new Response("Hello luxt")
+    return new Response(jsx.renderToString(this.init.imports["./route.ts"]), {
+      headers: {
+        "content-type": "text/html"
+      }
+    })
   }
 }
