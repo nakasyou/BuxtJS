0c6481f385d06db822fbe2acc52d2eb09c8a8ce0
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 11:46:36 +0900

    Update route.tsx

--- www/app/route.tsx
+++ www/app/route.tsx
@@ -1,7 +1,18 @@
 import { defineRoute } from "luxt"
 import { h, Fragment } from "luxt/jsx.ts"
-export default defineRoute(() => {
-  return <>
-    <h1>Hello luxt</h1>
-  </>
+
+export default defineRoute((c) => {
+  if (c.req.header('User-Agent').includes("Deno")) {
+    // isDeno
+    return c.redirect('/mod')
+  }
+  return <html>
+    <head>
+      <meta charset="utf-8" />
+    </head>
+    <body>
+      <h1>Luxt</h1>
+      <p>Luxtは、次世代のウェブフレームワークです。</p>
+    </body>
+  </html>
 })
