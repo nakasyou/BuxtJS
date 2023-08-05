e633194c0150a9e568d104a45b08a033766bcc40
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 13:29:43 +0900

    Update mod.ts

--- mods/cli/mod.ts
+++ mods/cli/mod.ts
@@ -10,13 +10,14 @@ import {
 interface Options {
   args: string[]
   config: object
+  projectRoot: string
 }
 export const cli = async (options: Options) => {
   const args = flags.parse(options.args)
 
   switch (args._[0]) {
     case "imports": {
-      const basePath = args._[1] || "."
+      const basePath = args._[1] || options.projectRoot
       console.log("Creating imports.ts..")
       await createImports({
         config: options.config,
