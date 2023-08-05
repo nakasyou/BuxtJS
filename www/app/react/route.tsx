691d6e1a18ced8cee6b4f830249432786c41874f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 15:28:51 +0900

    Update route.tsx

--- www/app/react/route.tsx
+++ www/app/react/route.tsx
@@ -2,6 +2,7 @@ import { defineRoute } from "luxt"
 import { h, Fragment } from "luxt/jsx.ts"
 import { ReactIsland } from "luxt/react.ts"
 import App from "./app.tsx"
+import * as path from "std/fs/mod.ts"
 
 export default defineRoute((c) => {
   return <html>
@@ -9,7 +10,7 @@ export default defineRoute((c) => {
       <meta charset="utf-8" />
     </head>
     <body>
-      <ReactIsland Island={App} />
+      <ReactIsland Island={App} path={path.join(path.fromFileUrl(import.meta.url),"./app.tsx")} />
     </body>
   </html>
 })
