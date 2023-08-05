c75fa7e1c94399467c20562e22fae69bfe8d4368
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 14:33:20 +0900

    Update react-island.tsx

--- mods/react/react-island.tsx
+++ mods/react/react-island.tsx
@@ -7,7 +7,7 @@ export interface ReactIslandProps {
   Island: any
 }
 export const ReactIsland = (props: Props) => {
-  console.log(<props.Island />)
+  console.log(<props.Island />, React.createElement("button", {},"count"))
   return (<>
     <div>Hello isLand!</div>
     <div id={`app-${Math.random()}`}>
