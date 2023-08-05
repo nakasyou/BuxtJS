e5bd1808adf5a458b646eaac47406a6f536da59a
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 12:30:05 +0900

    fix

--- mods/deps.ts
+++ mods/deps.ts
@@ -1,3 +1,3 @@
 export * as jsx from "https://deno.land/x/jsx@v0.1.5/mod.ts"
-export * as fs from "https://deno.land/std/fs@0.197.0/mod.ts"
-export * as path from "https://deno.land/std/path@0.197.0/mod.ts"
+export * as fs from "https://deno.land/std@0.197.0/fs/mod.ts"
+export * as path from "https://deno.land/std@0.197.0/path/mod.ts"
