0e6ab50b8d0b76e8115e39c8b1e9e9477be09b63
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 14:01:07 +0900

    Update mod.ts

--- mods/react/mod.ts
+++ mods/react/mod.ts
@@ -1,11 +1,3 @@
 // luxt/react.ts
-import { h, Fragment } from "luxt/jsx.ts"
 
-export interface ReactIslandProps {
-  island: any
-}
-export const ReactIsland = (props: Props) => {
-  return (<>
-    <div>Hello isLand!</div>
-  </>)
-}
+export * from "./react-islands.tsx"
