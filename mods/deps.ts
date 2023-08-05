29e261688609cc6f900b8d174a592a9de9474869
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 12:28:54 +0900

    Update deps.ts

--- mods/deps.ts
+++ mods/deps.ts
@@ -1,1 +1,3 @@
 export * as jsx from "https://deno.land/x/jsx@v0.1.5/mod.ts"
+export * as fs from "https://deno.land/std/fs@0.197.0/mod.ts"
+export * as path from "https://deno.land/std/path@0.197.0/mod.ts"
