3603cc22b0d4476ec725f344724b4294a91b90f5
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 13:43:47 +0900

    Create mod.ts

---
+++ mods/react/mod.ts
@@ -0,0 +1,11 @@
+// luxt/react.ts
+import { h, Fragment } from "luxt/jsx.ts"
+
+export interface ReactIslandProps {
+  island: any
+}
+export const ReactIsland = (props: Props) => {
+  return <>
+    <div>Hello isLand!</div>
+  </>
+}
