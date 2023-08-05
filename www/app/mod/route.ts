39560b514175757c51c1a8a76d9b46db72ca5da4
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 11:42:21 +0900

    Update route.ts

--- www/app/mod/route.ts
+++ www/app/mod/route.ts
@@ -2,5 +2,6 @@ import { defineRoute } from "luxt"
 
 export default defineRoute(c => {
   c.header("content-type", "application/typescript")
-  return c.body(`console.log(0)`)
+  
+  return c.body(`console.log(${JSON.stringify([...c.req.headers.entries()])})`)
 })
