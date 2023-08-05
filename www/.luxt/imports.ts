e2332d951b2fe720b8b3e7f3ae78a4d68e7b4584
nakasyou <syoutarou1103@outlook.jp>
2023-08-05 03:17:49 +0000

    imports

--- www/.luxt/imports.ts
+++ www/.luxt/imports.ts
@@ -2,11 +2,13 @@
 // You should not edit♡
 
 import $1 from '../app/api/route.tsx'
-import $2 from '../app/mod/route.ts'
-import $3 from '../app/route.tsx'
+import $2 from '../app/cli/[...path]/route.ts'
+import $3 from '../app/mod/route.ts'
+import $4 from '../app/route.tsx'
 
 export default {
   './api/route.tsx': $1,
-  './mod/route.ts': $2,
-  './route.tsx': $3,
+  './cli/[...path]/route.ts': $2,
+  './mod/route.ts': $3,
+  './route.tsx': $4,
 }
