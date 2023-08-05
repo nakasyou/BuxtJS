5dcb7e190d1138e15a138dd384b631fd8f2d261d
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 13:55:27 +0900

    Update route.tsx

--- www/app/react/route.tsx
+++ www/app/react/route.tsx
@@ -1,6 +1,7 @@
 import { defineRoute } from "luxt"
 import { h, Fragment } from "luxt/jsx.ts"
 import { ReactIsland } from "luxt/react.ts"
+import App from "./app.tsx"
 
 export default defineRoute((c) => {
   return <html>
@@ -8,7 +9,7 @@ export default defineRoute((c) => {
       <meta charset="utf-8" />
     </head>
     <body>
-      <ReactIsland island={0} />
+      <ReactIsland island={App} />
     </body>
   </html>
 })
