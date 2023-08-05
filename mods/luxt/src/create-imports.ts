3a920b34a827c999413f84cf31702388d453b4bf
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 10:29:17 +0900

    Create create-imports.ts

---
+++ mods/luxt/src/create-imports.ts
@@ -0,0 +1,18 @@
+import type { LuxtConfig } from "./config.ts"
+import * as fs from "std/fs/mod.ts"
+import * as path from "std/fs/mod.ts"
+
+export interface CreateImportsOptions {
+  basePath: string
+  config: LuxtConfig
+}
+export const createImports = async (options: CreateImportsOptions) => {
+  const routeGlob = path.join(options.basePath, "app", "**", "route.{js,ts,jsx,tsx}")
+  const getRoutesPromises: Promise<void> = []
+  for await (const entry of fs.expandGlob(routeGlob)) {
+    getRoutesPromises.push((async () => {
+      console.log(entry)
+    })())
+  }
+  await Promise.all(getRoutesPromises)
+}
