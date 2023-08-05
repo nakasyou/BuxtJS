0421316ff77d2aa94065ac9b8cd596f151d227ce
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 14:38:33 +0900

    Update app.tsx

--- www/app/react/app.tsx
+++ www/app/react/app.tsx
@@ -4,9 +4,5 @@
 import React, { useState } from "https://esm.sh/react@18.2.0"
 
 export default () => {
-  const [counter, setCounter] = useState(0)
-  const click = () => {
-    setCounter(counter+1)
-  }
   return React.createElement("button",{},"count")
 }
