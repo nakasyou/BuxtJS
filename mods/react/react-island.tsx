8310f4fcaa11e01aaab310c301334ee192ddf072
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 15:15:21 +0900

    Update react-island.tsx

--- mods/react/react-island.tsx
+++ mods/react/react-island.tsx
@@ -1,7 +1,7 @@
 // luxt/react.ts
 import { h, Fragment } from "luxt/jsx.ts"
-import { renderToString } from 'https://esm.sh/react-dom@18.2.0/server'
-import React from 'https://jspm.io/react'
+import { renderToString } from 'https://jspm.dev/react-dom@18.2.0/server'
+import React from 'https://jspm.dev/react'
 
 export interface ReactIslandProps {
   Island: any
