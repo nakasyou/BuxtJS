8e86c4829366e00dec0dc9ee8f05b019810f883f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 15:25:03 +0900

    async

--- mods/react/react-island.tsx
+++ mods/react/react-island.tsx
@@ -2,11 +2,12 @@
 import { h, Fragment } from "luxt/jsx.ts"
 import { renderToString } from 'https://jspm.dev/react-dom@18.2.0/server'
 import React from 'https://jspm.dev/react'
+import { build } from "https://deno.land/x/esbuild@v0.18.17/wasm.js"
 
 export interface ReactIslandProps {
   Island: any
 }
-export const ReactIsland = (props: Props) => {
+export const ReactIsland = async (props: Props) => {
   return (<>
     <div>Hello isLand!</div>
     <div id={`app-${Math.random()}`} dangerouslySetInnerHTML={{
