ca425fce546440e25b4e2a451bf359834467df2b
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 15:19:16 +0900

    Update react-island.tsx

--- mods/react/react-island.tsx
+++ mods/react/react-island.tsx
@@ -10,7 +10,7 @@ export const ReactIsland = (props: Props) => {
   return (<>
     <div>Hello isLand!</div>
     <div id={`app-${Math.random()}`} dangerouslySetInnerHTML={{
-      __html: renderToString(props.Island)
+      __html: renderToString(<props.Island />)
     }}></div>
   </>)
 }
