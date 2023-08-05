1dc3c6cb0ed4c507384eba72c7ef65b67fb9a1c4
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 12:18:59 +0900

    fix

--- www/app/cli/[...path]/route.ts
+++ www/app/cli/[...path]/route.ts
@@ -1,5 +1,5 @@
 import { defineRoute } from "luxt"
-import path from "std/fs/mod.ts"
+import * as path from "std/fs/mod.ts"
 
 export default defineRoute(async (c) => {
   const pathParam = c.req.param("path")
