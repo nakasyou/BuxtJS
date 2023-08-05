6f2bf3b06e1e5348f54491df1c489fda39069ad9
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 14:09:37 +0900

    Update react-island.tsx

--- mods/react/react-island.tsx
+++ mods/react/react-island.tsx
@@ -1,11 +1,16 @@
 // luxt/react.ts
 import { h, Fragment } from "luxt/jsx.ts"
+import { renderToString } from 'https://esm.sh/react-dom@18.2.0/server'
 
 export interface ReactIslandProps {
   island: any
 }
 export const ReactIsland = (props: Props) => {
+  props.island
   return (<>
     <div>Hello isLand!</div>
+    <div id={`app-${Math.random()}`}>
+      { renderToString(props.island) }
+    </div>
   </>)
 }
