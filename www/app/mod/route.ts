089cc7f40cbb7a3495403e10aff793ab91da06c1
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 11:38:12 +0900

    Update route.ts

--- www/app/mod/route.ts
+++ www/app/mod/route.ts
@@ -1,6 +1,6 @@
 import { defineRoute } from "luxt"
 
 export default defineRoute(c => {
-  c.header("content-type": "application/typescript")
+  c.header("content-type", "application/typescript")
   return c.body(`console.log(0)`)
 })
