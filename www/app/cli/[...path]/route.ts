a92128eecaef9dd992fd25dc99f7979b73fbcca9
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-05 12:38:22 +0900

    Update route.ts

--- www/app/cli/[...path]/route.ts
+++ www/app/cli/[...path]/route.ts
@@ -3,7 +3,7 @@ import * as path from "std/path/mod.ts"
 
 export default defineRoute(async (c) => {
   const pathParam = c.req.param("path")
-  const readPath = path.join("./mods/cli",pathParam)
+  const readPath = path.join("./mods",pathParam)
   c.header("content-type", "application/typescript")
   return c.body(await Deno.readTextFile(readPath))
 })
