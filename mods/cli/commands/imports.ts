525cc6a4dfdff90ad18a3c3a2bf1129de10bd67e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-07 17:16:14 +0900

    Update imports.ts

--- mods/cli/commands/imports.ts
+++ mods/cli/commands/imports.ts
@@ -1,4 +1,7 @@
 import { type CliCommand } from "../mod.ts"
+import {
+  createImports,
+} from "../luxt/src/create-imports.ts"
 
 export default async (init: CliCommand) => {
   const basePath = init.args._[1] || init.projectRoot
