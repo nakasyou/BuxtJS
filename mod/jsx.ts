f6913fe9ef6d0ca11bd0006c16f6ee2c23b2f69f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-07 16:53:14 +0900

    Update jsx.ts

--- mod/jsx.ts
+++ mod/jsx.ts
@@ -1,9 +1,1 @@
-import {
-  jsx
-} from "../mods/jsx/mod.ts"
-
-export const Fragment = jsx.Fragment
-export const h = jsx.h
-export const renderToString = jsx.renderToString
-export type Component = jsx.Component
-export type Node = jsx.Node
+export * from "../mods/jsx/mod.ts"
