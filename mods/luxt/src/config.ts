cba5ee9452bdb10a5328e6d67beb2bf2be275d2c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-09 15:38:45 +0900

    Update config.ts

--- mods/luxt/src/config.ts
+++ mods/luxt/src/config.ts
@@ -1,6 +1,10 @@
 import type { Imports } from "../types/route.ts"
+import { unplugin } from "../deps.ts"
+
+export interface LuxtPlugin = (...args: any[]) => unplugin.UnpluginInstance
 
 export interface LuxtConfig {
   imports: Imports
+  plugins: LuxtPlugin[]
 }
 export const defineConfig = (config: LuxtConfig): LuxtConfig => config
